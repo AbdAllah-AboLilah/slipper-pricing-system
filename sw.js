d6cf7a991dc3a1f3b463f61db2a7ad5f6d991a9b
AbdAllah-AboLilah <dodododo054@gmail.com>
2026-08-15 11:55:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='1.3.9';
+const VERSION='1.3.10';
 const CACHE=`slipper-pricing-${VERSION}`;
 const CORE=['./','./index.html','./manifest.webmanifest','./src/styles.css','./src/app.js','./src/core/db.js','./src/core/state.js','./src/core/utils.js','./src/modules/invoices.js','./src/modules/pricing.js','./src/modules/catalog.js','./src/modules/settings.js','./src/modules/export.js','./src/modules/receipt.js','./src/modules/backup.js','./src/modules/version.js','./assets/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
