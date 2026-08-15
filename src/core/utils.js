8241bbc13a46203aae327651e3e2b32fdec22a8a
AbdAllah-AboLilah <dodododo054@gmail.com>
2026-08-15 11:34:18 +0300

    Add files via upload

--- src/core/utils.js
+++ src/core/utils.js
@@ -1,4 +1,4 @@
-export const APP_VERSION='1.3.8';
+export const APP_VERSION='1.3.9';
 export const CREATOR='عبد الله <Abo Lilah>';
 export const BUILD_DATE='2026-08-13';
 export const APP_NAME='نظام تسعير السليبر';
